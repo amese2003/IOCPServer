46520961bba8586b67551f498e3b86670a32a285
용주 곽 <amese1225@gmail.com>
2021-06-23 21:52:59 +0900

    스레드 생성

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -2,8 +2,53 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread() 
+{
+	cout << "Hello Thread" << endl;
+}
+
+void HelloThread2(int32 num)
+{
+	cout << num << endl;
+}
+
 int main()
 {
-	HelloWorld();
+	vector<std::thread> v;
+	//v.resize(10);
+
+	//std::thread t;
+	//auto id1 = t.get_id();	// 스레드 마다 id;;
+
+
+	//t = std::thread(HelloThread);
+
+	//int32 count = t.hardware_concurrency(); // cpu 코어 개수?
+	//auto id2 = t.get_id();	// 스레드 마다 id
+
+	//std::thread t2;
+
+	//t2 = std::thread(HelloThread2, 1);
+	//
+	////t.detach(); // std::thread 객체에서 실제 스레드를 분리\
+
+
+	//if (t.joinable())
+	//	t.join();
+
+	//t2.join();
+
+	for (int32 i = 0; i < 10; i++) {
+		v.push_back(std::thread(HelloThread2, i));
+	}
+
+	for (int32 i = 0; i < 10; i++) {
+		if (v[i].joinable())
+			v[i].join();
+	}
+
+	cout << "Hello Main" << endl;
 }
 
--- Server/ServerCore/CorePch.cpp
+++ Server/ServerCore/CorePch.cpp
@@ -3,5 +3,4 @@
 
 void HelloWorld()
 {
-	cout << "Hello Server!" << endl;
 }
