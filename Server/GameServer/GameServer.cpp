08d61e2fa4caddb0b0e57c223cf2af7b799fb7ed
용주 곽 <amese1225@gmail.com>
2021-08-02 20:32:06 +0900

    Lock Free Stack #3

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,45 +1,46 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
 #include <atomic>
 #include <mutex>
-#include <windows.h>
+#include <Windows.h>
 #include <future>
-#include "ConcurrentQueue.h"
 #include "ConcurrentStack.h"
+#include "ConcurrentQueue.h"
 
 LockQueue<int32> q;
 LockFreeStack<int32> s;
 
-void Push()
-{
-	while (true)
-	{
-		int32 value = rand() % 100;
+void Push() {
+	while (true) {
+		int32 value = rand() & 100;
 		s.Push(value);
 
-		this_thread::sleep_for(10ms);
+		//this_thread::sleep_for(10ms);
 	}
 }
 
-void Pop()
-{
-	while (true)
-	{
-		int32 data = 0;
-		if (s.TryPop(OUT data))
-			cout << data << endl;
+void Pop() {
+	while (true) {
+		auto data = s.TryPop();
+
+		if (data != nullptr)
+			cout << (*data) << endl;
 	}
 }
-
 
 int main()
 {
+	/*shared_ptr<int32> ptr;
+	bool value = atomic_is_lock_free(&ptr);*/
+
 	thread t1(Push);
 	thread t2(Pop);
-	thread t3(Pop);
+	//thread t3(Pop);
 
 	t1.join();
 	t2.join();
-	t3.join();
+	//t3.join();
 }
+
