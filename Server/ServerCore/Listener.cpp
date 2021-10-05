56e5b65c4bcba8459f0a555b9ed60781be222061
Yong Ju Kwak <amese1225@gmail.com>
2021-10-05 23:10:33 +0900

    Session #1

--- Server/ServerCore/Listener.cpp
+++ Server/ServerCore/Listener.cpp
@@ -123,10 +123,8 @@ void Listener::ProcessAccept(AcceptEvent* acceptEvent)
 	}
 
 	session->SetNetAddress(NetAddress(sockAddress));
+	session->ProcessConnect();
 
-	cout << "Client Connected!" << endl;
-
-	// TODO
 
 	RegisterAccept(acceptEvent);
 }
